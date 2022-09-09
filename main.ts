4e87e1d2df19c7ed71b4857d469e21ef7d0df18b
DanDC25 <111406215+DanDC25@users.noreply.github.com>
2022-09-09 09:02:24 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,42 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showString("" + (receivedNumber))
-    basic.showLeds(`
-        # # # . .
-        # . . # .
-        # . . # .
-        # . . # .
-        # # # . .
-        `)
-    music.playMelody("C5 C5 - - - - - - ", 120)
-})
 input.onButtonPressed(Button.A, function () {
     Starter = false
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-    basic.showLeds(`
-        . # # # .
-        # . . . .
-        # . . . .
-        # . . . .
-        . # # # .
-        `)
-    music.playMelody("- - - - - - C C ", 120)
+    if (receivedString == Clean) {
+        basic.showString("Clean")
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . . . .
+            # . . . .
+            . # # # .
+            `)
+        music.playMelody("- - - - - - C C ", 120)
+    } else if (receivedString == Dirty) {
+        basic.showString("Dirty")
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # . . # .
+            # . . # .
+            # # # . .
+            `)
+        music.playMelody("C5 C5 - - - - - - ", 120)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     Starter = true
 })
 let Starter = false
+let Dirty = ""
+let Clean = ""
 if (true) {
+    Clean = "Clean!"
+    Dirty = "Dirty!"
     radio.setGroup(1)
     if (input.lightLevel() > 120) {
-        radio.sendString("Clean")
+        radio.sendString("" + (Clean))
     } else if (input.lightLevel() < 120) {
-        radio.sendNumber(1)
+        radio.sendString("" + (Dirty))
     }
-} else {
-    basic.clearScreen()
 }
